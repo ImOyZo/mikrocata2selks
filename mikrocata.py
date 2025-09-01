ca5176793381b7532c3e689849564f6b463dec04
ImOyZo <96724899+ImOyZo@users.noreply.github.com>
2025-09-01 22:56:33 +0800

    Add files via upload

--- mikrocata.py
+++ mikrocata.py
@@ -472,7 +472,7 @@ def connect_to_tik():
                     ctx = ssl.create_default_context()
                     ctx.check_hostname = False
                     ctx.verify_mode = ssl.CERT_NONE
-                    ctx.set_ciphers('DEFAULT@SECLEVEL=0')
+                    ctx.set_ciphers('DEFAULT@SECLEVEL=2')
                 else:
                     # Settings for valid certificates
                     ctx = ssl.create_default_context()
